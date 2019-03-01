09907c06e04f2bb7a84956e029807d2880e8b7a7
ubuntu <1@1.ru>
2019-03-01 14:55:57 +0400

    start project

--- django_mytweets/django_mytweets/settings.py
+++ django_mytweets/django_mytweets/settings.py
@@ -81,3 +81,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     # put absolute path here as string not relative path.
+#     # forward slash to be used even in windows.
+#     os.path.join(os.path.dirname(__file__),
+#                  'static',
+#                  ),
+# )
+STATICFILES_DIRS = (
+    os.path.join(
+        os.path.dirname(__file__),
+        'static',
+    ),
+)
